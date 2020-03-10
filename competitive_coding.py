e3b846250edf4a8e361c61efe129eece3b90d690
Shubham Karma <shubh.karma001@gmail.com>
2020-03-10 18:32:36 +0530

    Update competitive_coding.py
    Added code with enumerate

--- competitive_coding.py
+++ competitive_coding.py
@@ -1,9 +1,10 @@
 import numpy
-# This statement takes input of two integers separated by space then splits it to get dimensions of array.
+
+'''Here we take input of two integers separated by space then splits it to get dimensions of array.'''
 # numpy.eye can make a identity matrix which we are converting to string and then applying replace to get output in particular format
 print(str(numpy.eye(*map(int,input().split()))).replace('0',' 0').replace('1',' 1'))
 
-# Below lines are used to read input data and then we can perform operations on them
+'''This program is used to read input data and then we can perform operations on them'''
 # If we are working with Python 2 then use raw_input()
 # Sample input: 
 # The first line contains two space separated integers, n and m.
@@ -12,7 +13,25 @@ print(str(numpy.eye(*map(int,input().split()))).replace('0',' 0').replace('1',' 
 # 1 4
 # 4 5 6 7
 # 7 8 4 6
+# List comprehension is used
 import numpy as np
 n, m = map(int, input().split())
 a, b = (np.array([input().split() for _ in range(n)], dtype=int) for _ in range(2))
 print(a+b, a-b, a*b, a//b, a%b, a**b, sep='\n')
+
+'''Below code helps to find the runner up from a list of scores'''
+# Given n scores 
+n = int(raw_input())
+arr = map(int, raw_input().split())
+
+# enumerate can work with list indexes
+if 2<=n<=10:
+    arr.sort(reverse=True)
+    for i,j in enumerate(arr):
+        if arr[i+1]!=j:
+            print(arr[i+1])
+            break
+''''''
+            
+            
+       
